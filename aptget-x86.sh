f9636d3f6bec7d1c884b21ef22b1c89aa1d8f597
Shannon Spires <svs@bearlanding.com>
2018-02-07 16:43:29 -0700

    Get mercurial first

--- aptget-x86.sh
+++ aptget-x86.sh
@@ -3,11 +3,11 @@
 # run this as root from your (user-level) home directory
 # Set the date on the system first, if it's not already set
 apt-get update
+apt-get install -y mercurial
 apt-get install -y emacs
 apt-get install -y git
 apt-get install -y build-essential
 apt-get install -y m4
-apt-get install -y mercurial
 apt-get install -y rlwrap
 apt-get install -y host
 apt-get install -y locate
