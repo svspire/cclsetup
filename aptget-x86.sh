eacd79d1a51be73968a06b74129f6d4c661ceed6
Shannon Spires <svs@bearlanding.com>
2018-02-07 16:38:36 -0700

    Updated script for git vs. svn

--- aptget-x86.sh
+++ aptget-x86.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 # Installs CCL on x86 Linux
 # run this as root from your (user-level) home directory
-# Set the date on the system first, if it's not already
+# Set the date on the system first, if it's not already set
 apt-get update
 apt-get install -y emacs
+apt-get install -y git
 apt-get install -y build-essential
 apt-get install -y m4
-apt-get install -y subversion
-apt-get install -y git
 apt-get install -y mercurial
 apt-get install -y rlwrap
 apt-get install -y host
@@ -18,6 +17,6 @@ cd
 wget http://beta.quicklisp.org/quicklisp.lisp
 
 cd /usr/local/src/
-svn co http://svn.clozure.com/publicsvn/openmcl/trunk/linuxx86/ccl
+git clone https://github.com/Clozure/ccl/releases/download/v1.11.5/ccl-1.11.5-linuxx86.tar.gz
 
 # now run setupccl64.sh or setupccl32.sh
