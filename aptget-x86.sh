521f54aa74867cb64430d9a8039a975242149444
Shannon Spires <ssos@bearlanding.com>
2022-08-16 13:56:25 -0600

    Update to proper CCL release number.
    Don't install mercurial any more since I've stopped using it.

--- aptget-x86.sh
+++ aptget-x86.sh
@@ -3,7 +3,7 @@
 # run this as root from your (user-level) home directory
 # Set the date on the system first, if it's not already set
 apt-get update
-apt-get install -y mercurial
+# apt-get install -y mercurial # I'm not using mercurial any  more
 apt-get install -y emacs
 apt-get install -y git
 apt-get install -y build-essential
@@ -21,8 +21,8 @@ fi
 if [ ! -d "/usr/local/src/ccl" ]; then
   mkdir /usr/local/src/ccl
   cd /usr/local/src/ccl
-  wget https://github.com/Clozure/ccl/releases/download/v1.12-dev.5/linuxx86.tar.gz
-  tar xvf linuxx86.tar.gz
+  wget https://github.com/Clozure/ccl/releases/download/ccl-1.12.1-linuxx86.tar.gz
+  tar xvf ccl-1.12.1-linuxx86.tar.gz
 fi
 
 # git clone https://github.com/Clozure/ccl/releases/download/v1.11.5/ccl-1.11.5-linuxx86.tar.gz
